9847ef23765d03a30389dfafbdbab48fb0961966
I-Tai <smes93434@gmail.com>
2023-04-30 12:45:43 -1000

    Adding require products

--- app.js
+++ app.js
@@ -2,13 +2,10 @@
 const express = require('express');  //importing the Express library
 const app = express(); //calling the express() function.
 
+const productRoutes = require('./api/route/products');
+
 //When the server eceives a request,  The middleware function sets the response status to 
 //200 and returns a JSON object with a message property set to "It works!".
-app.use((req, res, next)=> {   //The app.use() function is used to define middleware functions for the application
-    res.status(200).json({
-        message: 'It works!'   
-    });
-
-})
+app.use('/products', productRoutes);
 
 module.exports = app; //allows other parts of the application to use this server configuration.
