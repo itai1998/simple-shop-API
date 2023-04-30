565af0822812777885d2b845b21b6814dd06c91c
I-Tai <smes93434@gmail.com>
2023-04-30 11:42:20 -1000

    Add commons

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const express = require('express');
-const app = express();
+//using the Express framework
+const express = require('express');  //importing the Express library
+const app = express(); //calling the express() function.
 
-app.use((req, res, next)=> {
+//When the server eceives a request,  The middleware function sets the response status to 
+//200 and returns a JSON object with a message property set to "It works!".
+app.use((req, res, next)=> {   //The app.use() function is used to define middleware functions for the application
     res.status(200).json({
-        message: 'It works!'
+        message: 'It works!'   
     });
 
 })
 
-module.exports = app;
+module.exports = app; //allows other parts of the application to use this server configuration.
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-const http = require('http');
-const app = require('./app');
+//imports the built-in http module, which provides functionality for creating and managing HTTP servers and clients.
+const http = require('http'); 
 
-const port = process.env.PORT || 3000;
+//imports the contents of app module, which contains the definition of an Express application.
+const app = require('./app');  
 
-const server = http.createServer(app);
+//sets its value to the value of the environment variable process.env.PORT, if it exists. Otherwise, it sets the port number to 3000
+const port = process.env.PORT || 3000; 
 
+//creates a new HTTP server using the createServer(). The app object will handle all incoming requests to the server.
+const server = http.createServer(app); 
+
+//When the server receives a request, it will call the app object to handle the request. 
 server.listen(port);
